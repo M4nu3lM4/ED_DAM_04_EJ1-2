2178923312e8c7c9169d879045d5234c964a8052
Manuel_mma <manumaral04@gmail.com>
2024-02-14 18:08:26 +0100

    Clase CostePersonal.java refactorizada

--- CostePersonal.java
+++ CostePersonal.java
@@ -1,36 +1,17 @@
-1
- 2
- 3
- 4
- 5
- 6
- 7
- 8
- 9
-10
-11
-12
-13
-14
-15
-16
-17
-18
 public class CostePersonal {
  
-	static float CosteDelPersonal(Trabajador trabajadores[]) {
-		float costeFinal = 0;
-		Trabajador trabajador;
-		for (int i = 0; y < trabajadores.length; i++) {
-			trabajador = trabajadores[i];
-			if (trabajador.getTipoTrabajador() == Trabajador.DIRECTOR|| trabajador.getTipoTrabajador() == Trabajador.SUBDIRECTOR) {
-					costeFinal += trabajador.getNomina();
-			}
-			else
-			{
-				costeFinal += trabajador.getNomina() + (trabajador.getHorasExtras() * 20);
-			}
-		}
-		return costeFinal;
-	}
+    static float calcularCostePersonal(Trabajador trabajadores[]) {
+        float costeFinal = 0;
+        Trabajador currentWorker;
+        for (int i = 0; i < trabajadores.length; i++) {
+            currentWorker = trabajadores[i];
+            if (currentWorker.getTipoTrabajador() == Trabajador.DIRECTOR || currentWorker.getTipoTrabajador() == Trabajador.SUBDIRECTOR) {
+                costeFinal += currentWorker.getNomina();
+            } else {
+                costeFinal += currentWorker.getNomina() + (currentWorker.getHorasExtras() * 20);
+            }
+        }
+        return costeFinal;
+    }
+}
 
